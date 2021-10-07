5037eec1d0175be013b95265f9599fc1863fa842
René Schwaiger <sanssecours@me.com>
2021-10-07 18:59:21 +0200

    Examples: Add CSV conversion script

---
+++ Source/convert.py
@@ -0,0 +1,45 @@
+# -- Imports ------------------------------------------------------------------
+
+from csv import DictWriter
+from pathlib import Path
+from re import compile
+
+# -- Functions ----------------------------------------------------------------
+
+
+def read_log(filepath):
+    """Read acceleration log data"""
+
+    line_regex = compile(r"\[I\]\s*\((?P<ms>\d+)ms\)[^\d]+(?P<counter>\d+)"
+                         r"[^\d]+(?P<timestamp>\d+(\.\d+)?)[^d]+"
+                         r"(?P<acceleration>\d+);")
+    values = []
+    with open(filepath) as file:
+        for line in file:
+            match = line_regex.match(line)
+            if match:
+                values.append({
+                    'Millisecond': match['ms'],
+                    'Counter': match['counter'],
+                    'Timestamp': match['timestamp'],
+                    'Acceleration': match['acceleration']
+                })
+
+    return values
+
+
+def convert_csv(filepath, values):
+    """Convert acceleration data to CSV file"""
+
+    with open(Path(filepath).with_suffix(".csv"), 'w', newline='') as csvfile:
+        fieldnames = ['Millisecond', 'Counter', 'Timestamp', 'Acceleration']
+        writer = DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        writer.writerows(values)
+
+
+if __name__ == '__main__':
+    filepath = "Data/Log.txt"
+    values = read_log(filepath)
+    convert_csv(filepath, values)
