4e763a0cfa87784ac3996e0c33b8d0ce9011853d
René Schwaiger <sanssecours@me.com>
2021-10-11 17:10:50 +0200

    Examples: Add code to store data in HDF5 file

--- Source/convert.py
+++ Source/convert.py
@@ -4,6 +4,9 @@ from csv import DictWriter
 from pathlib import Path
 from re import compile
 
+from h5py import File
+from numpy import asarray, recarray
+
 # -- Functions ----------------------------------------------------------------
 
 
@@ -28,8 +31,29 @@ def read_log(filepath):
     return values
 
 
+def convert_values_rows(values):
+    """Return acceleration data as rows (instead of lines)"""
+
+    miliseconds = []
+    counters = []
+    timestamps = []
+    acceleration = []
+    for value in values:
+        miliseconds.append(value['millisecond'])
+        counters.append(value['counter'])
+        timestamps.append(value['timestamp'])
+        acceleration.append(value['acceleration'])
+
+    return {
+        'millisecond': miliseconds,
+        'counter': counters,
+        'timestamp': timestamps,
+        'acceleration': acceleration
+    }
+
+
 def convert_csv(filepath, values):
-    """Convert acceleration data to CSV file"""
+    """Store acceleration data in CSV file"""
 
     with open(Path(filepath).with_suffix(".csv"), 'w', newline='') as csvfile:
         fieldnames = ['millisecond', 'counter', 'timestamp', 'acceleration']
@@ -39,7 +63,23 @@ def convert_csv(filepath, values):
         writer.writerows(values)
 
 
+def convert_hdf(filepath, values):
+    """Store acceleration data in HDF5 file"""
+
+    types = [('millisecond', int), ('counter', int), ('timestamp', float),
+             ('acceleration', int)]
+    number_lines = len(values['millisecond'])
+    data = recarray(number_lines, dtype=types)
+    data['millisecond'] = asarray(values['millisecond'])
+    data['counter'] = asarray(values['counter'])
+    data['timestamp'] = asarray(values['timestamp'])
+    data['acceleration'] = asarray(values['acceleration'])
+    with File(Path(filepath).with_suffix(".hfd5"), 'w') as hdf:
+        hdf.create_dataset("acceleration", data=data, shape=(number_lines, ))
+
+
 if __name__ == '__main__':
     filepath = "Data/Log.txt"
     values = read_log(filepath)
     convert_csv(filepath, values)
+    convert_hdf(filepath, convert_values_rows(values))
